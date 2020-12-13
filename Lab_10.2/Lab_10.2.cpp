b4f97401b7af98e1efc2457cc1b0253b5835ee3e
Woloda <dorozovecvoloda@gmail.com>
2020-12-13 19:28:10 +0200

    Lab__10_2

---
+++ Lab_10.2/Lab_10.2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+
+using namespace std;
+
+int kil_sliv(char* fname);
+
+int main(void)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char fname[51];
+	cout << "Введіть назву фізичного файлу: ";
+	cin >> fname;
+	cout << endl;
+
+	int lichilnuk = kil_sliv(fname);
+
+	cout << "В цьому реченні слів: " << lichilnuk << endl;
+
+	return 0;
+}
+
+int kil_sliv(char* fname)
+{
+	ifstream fin(fname);
+
+	string lit_r;
+
+	int lichilnuk = 0;
+	while (getline(fin, lit_r))
+	{
+		cout << "Літерний рядок зчитаний з фізичного файлу: ";
+		cout << lit_r << endl << endl;
+
+		size_t pos = 0;
+		size_t p = 0;
+		p = lit_r.find(" ", pos);
+
+		int l = 0;
+		while (p < lit_r.length() - 1)
+		{
+			p = lit_r.find(" ", ++l);
+			if (p > pos + 1)
+				lichilnuk++;
+			pos = p;
+		}
+	}
+	return lichilnuk;
+}
---
+++ UnitTest(Lab_10.2)/UnitTest(Lab_10.2).cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_10.2/Lab_10.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab102
+{
+	TEST_CLASS(UnitTestLab102)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[101] = "E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_10.2\\Lab_10.2\\test.txt";
+
+			int lichilnuk = kil_sliv(fname);
+
+			Assert::AreEqual(lichilnuk, 5);
+		}
+	};
+}
